b2ac5e12e93237f8c836c92da7f640fa07d6db81
onur koc <onurkottz@gmail.com>
2022-09-18 16:59:10 +0300

    audio

---
+++ js.js
@@ -0,0 +1,34 @@
+let cardhead = document.querySelector(".cardhead");
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (i = 0; i < data.length; i++) {
+      let cardiv = document.createElement("div");
+
+      cardhead.appendChild(cardiv);
+
+      var audio = new Audio(`${data[i].sound}`);
+      console.log(audio);
+
+      
+      
+      
+      cardiv.innerHTML = `<div class="card">
+        <div class="picture"><img src=${data[i].picture} alt=${data[i].name} /></div>
+        <div class="header">${data[i].name}</div>
+        <div class="explain">
+          <div class="color">${data[i].color}</div>
+        </div>
+        <audio controls>
+        <source src=${data[i].sound} type="audio/mpeg">
+        </audio>
+        <div>
+        </div>
+      </div>
+      
+      `;
+
+      
+    }
+  });
